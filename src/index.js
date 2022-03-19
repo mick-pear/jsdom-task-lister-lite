00a74da4b9a19809fa8c958b80ef77ab09900a25
Matthew <matthew_mcaleer@hotmail.com>
2022-03-19 23:11:08 +1100

    done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+    const form = document.querySelector("#create-task-form")
+    const task = document.getElementById('tasks');
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        event.target.taskDescription;
+        //console.log(taskDescription.value);
+        let taskListItem = document.createElement("li");
+        task.appendChild(taskListItem);
+        taskListItem.innerHTML = `${taskDescription.value} <button>x</button>`;
+    })
+
+    task.addEventListener("click", (event) => {
+        const trigger = event.target
+        if (trigger.tagName === 'BUTTON') {
+           trigger.parentNode.remove();
+    }});
 });
